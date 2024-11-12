e429808228f1197e3556f7898c55a761afb90902
龙一野 <2226548812@qq.com>
2024-11-12 21:07:55 +0800

    数据库表设计

--- src/main/java/com/yiye/entity/User.java
+++ src/main/java/com/yiye/entity/User.java
@@ -1,10 +1,14 @@
 package com.yiye.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.Date;
 
 @Data
 @Builder
@@ -14,22 +18,63 @@ import lombok.NoArgsConstructor;
 public class User {
 
     /**
-     * 用户 id
+     * id
      */
+    @TableId(value = "id", type = IdType.AUTO)
     private Long id;
 
     /**
-     * 姓名
+     * 用户昵称
      */
-    private String name;
+    private String userName;
 
     /**
-     * 年龄
+     * 账号
      */
-    private Integer age;
+    private String userAccount;
+
+    /**
+     * 用户头像
+     */
+    private String avatarUrl;
+
+    /**
+     * 性别
+     */
+    private Byte gender;
+
+    /**
+     * 密码
+     */
+    private String userPassword;
+
+    /**
+     * 电话
+     */
+    private String phone;
 
     /**
      * 邮箱
      */
     private String email;
+
+    /**
+     * 状态：0 正常
+     */
+    private Integer userStatus;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 更新时间
+     */
+    private Date updateTime;
+
+    /**
+     * 是否删除
+     */
+    private Byte isDelete;
 }
